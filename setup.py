5a2800547dbb27574589246d27f3154bce694a89
shuyan.ycf <shuyan.ycf@antgroup.com>
2026-04-03 20:28:09 +0800

    Normalize packaging metadata and build configuration

--- setup.py
+++ setup.py
@@ -204,10 +204,6 @@ ext_modules.append(
 
 setup(
     name="cula",
-    use_scm_version={
-        "write_to": "cula/_version.py",
-        "local_scheme": "node-and-date",
-    },
     packages=find_packages(include=["cula", "cula.*"]),
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension},
